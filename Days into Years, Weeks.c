25932d0371b0ee5f12afd6295273f6ac216c8cd2
Sriram939 <nadakudurusriram@gmail.com>
2024-10-13 18:03:50 +0530

    Uploading code: Days into Years, Weeks.c

---
+++ Days into Years, Weeks.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int D;
+    scanf("%d", &D);
+    int years = D / 365;
+    int remaining_days = D % 365;
+    int weeks = remaining_days / 7;
+    printf("%d\n", years);
+    printf("%d\n", weeks);
+    return 0;
+}
